08d2fc0ae4012255e845c4a5adbf1265ddc0a1e6
Felix <fcanino1@babson.edu>
2015-11-15 13:55:53 -0500

    modified:   DataGrabber.py

--- DataGrabber.py
+++ DataGrabber.py
@@ -3,37 +3,48 @@ import Tkinter
 import tkFileDialog
 import pandas as pd
 
-i = 0
+def DataGrabber():
 
-filetypes = {1: "df = pd.read_csv(filename)", 2: "df = pd.read_excel(filename)",}
+	i = 0
 
-while i <= 0:
+	filetypes = {1: "df = pd.read_csv(filename)", 2: "df = pd.read_excel(filename,sheet)",3: "df = pd.read_html(filename)"}
 	
-	quit_choice = 3
+	while i <= 0:
 	
-	print "Choose the number that corresponds to the file type that you will be opening: "
-	print """
-	1: CSV File 
-	2: Excel 2003 File (XLS / XLSX)
-	3: Exit FileGrabber
-	"""
-	type_choice = int(raw_input("> "))
-	if type_choice == quit_choice:
-		quit()
-	else:
-		for typ in filetypes:
-			if type_choice == typ:
-				check = True
-				break
-		if check == True:
-			i += 1
+		quit_choice = 4
+	
+		print "Choose the number that corresponds to the file type that you will be opening: "
+		print """
+		1: CSV File 
+		2: Excel 2003 File (XLS / XLSX)
+		3: Webpage Table (HTML)
+		4: Exit FileGrabber
+		"""
+		choice = int(raw_input("> "))
+		if choice == quit_choice:
+			quit()
 		else:
-			print "Invalid choice selected.  Please enter a valid option."
+			for typ in filetypes:
+				if choice == typ:
+					check = True
+					break
+				else:
+					check = False
+			if check == True:
+				i += 1
+			else:
+				print "Invalid choice selected.  Please enter a valid option."
 	
-root = Tkinter.Tk() ; root.withdraw()
+	root = Tkinter.Tk() ; root.withdraw()
 
-filename = tkFileDialog.askopenfilename(parent=root,title="FileGrabber (Beta)")
-open_str = filetypes[type_choice]
-exec open_str
+	filename = tkFileDialog.askopenfilename(parent=root,title="DataGrabber v0.1")
+	exec_str = filetypes[choice]
+	exec exec_str
+
+	return df
+
+df = DataGrabber()
 
 print df
+print type(df)
+
